abea821b7cac3f3ff1951d40674285d9061c1198
AslamtoIbrahim <aslamtoibrahim@gmail.com>
2024-12-28 12:30:18 +0100

    [FEAT] Show labels above bars while hovering them

--- script.js
+++ script.js
@@ -2,21 +2,52 @@ const dayBars = document.querySelectorAll('.day-bar');
 const bars = document.querySelectorAll('.bar');
 const totalMonth = document.getElementById('total-month');
 let total = 0;
+ 
 
 fetch('data.json')
     .then(response => response.json())
     .then(data => {
+        let maxAmount = Math.max(...data.map(item => item.amount));
         dayBars.forEach((element, index) => {
             element.querySelector('.day').textContent = data[index].day;
             element.querySelector('.amount').textContent = '$' + data[index].amount;
             element.querySelector('.bar').style.height = data[index].amount * 2 + 'px';
-            element.querySelector('.bar').addEventListener('click', function () {
-                this.classList.toggle('changeColor');
-                element.querySelector('.amount').classList.toggle('showAmount');
-            })
+
+            // show and hide the amount label above
+            showHoverElement(element.querySelector('.bar'), element.querySelector('.amount'));
+
+            if (maxAmount ===  data[index].amount) {
+                // change the biggest amount on the chart into another color
+                element.querySelector('.bar').style.backgroundColor = 'hsl(186, 34%, 60%)';
+                // change the hover color on the chart to this biggest bar
+                changeHoverColor(element.querySelector('.bar'));
+            }
+
             total += Number(data[index].amount * 2);
         })
+
 
         totalMonth.textContent = '$' + total;
     })
     .catch(error => console.error('Error: ', error));
+
+
+    // change the color of the bar on hover while keeping the other bars in the same color
+    function changeHoverColor(element){
+        element.addEventListener('mouseover', () => {
+            element.style.backgroundColor = '#a3dbe1';
+        });
+        element.addEventListener('mouseout', () => {
+            element.style.backgroundColor = 'hsl(186, 34%, 60%)';
+        });
+    }
+
+    // show an element while hovering another
+    function showHoverElement(element, hideElement) {
+        element.addEventListener('mouseover', () => {
+            hideElement.style.display = 'block';
+        });
+        element.addEventListener('mouseout', () => {
+            hideElement.style.display = 'none';
+        });
+    }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         'soft-red': 'hsl(10, 79%, 65%)',
         'very-soft-red': 'hsl(10deg 93.95% 76.43%)',
         'cyan': 'hsl(186, 34%, 60%)',
+        'soft-cyan': '#abecf3',
         'dark-brown': 'hsl(25, 47%, 15%)',
         'medium-brown': 'hsl(28, 10%, 53%)',
         'cream': ' hsl(27, 66%, 92%)',
